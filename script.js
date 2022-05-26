7b3c1e296ada773814824f0df1a61306ed027fd3
belie369 <katarzyna.derbich@gmail.com>
2022-05-26 12:11:08 +0200

    Added a checkClick function; corrected a before method to append

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ const prepareDOMElements = () => {
 const prepareDOMEvents = () => {
 	addBtn.addEventListener('click', addNewTask);
 	todoInput.addEventListener('keyup', enterCheck);
+	ulList.addEventListener('click', checkClick);
 };
 
 const addNewTask = () => {
@@ -44,7 +45,7 @@ const addNewTask = () => {
 		newTask = document.createElement('li');
 		newTask.textContent = todoInput.value;
 		newTask.setAttribute('id', `todo-${idNumber}`);
-		ulList.before(newTask);
+		ulList.append(newTask);
 
 		todoInput.value = '';
 		alertInfo.textContent = '';
@@ -78,12 +79,19 @@ const createToolsArea = () => {
 	deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
 
 	toolsPanel.append(completeBtn, editBtn, deleteBtn);
+};
 
-	/* <div class="tools">
-<button class="complete"><i class="fas fa-check"></i></button>
-<button class="edit">EDIT</button>
-<button class="delete"><i class="fas fa-times"></i></button>
-</div> */
+const checkClick = (e) => {
+	if (e.target.classList.value !== '') {
+		if (e.target.closest('button').classList.contains('complete')) {
+			e.target.closest('li').classList.toggle('completed');
+			e.target.closest('button').classList.toggle('completed');
+		} else if (e.target.closest('button').classList.contains('edit')) {
+			console.log('edit');
+		} else if (e.target.closest('button').classList.contains('delete')) {
+			console.log('delete');
+		}
+	}
 };
 
 document.addEventListener('DOMContentLoaded', main);
