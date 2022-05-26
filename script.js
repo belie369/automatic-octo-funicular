a7e9ed38851db98ea7c443b224d875377646f0be
belie369 <katarzyna.derbich@gmail.com>
2022-05-26 13:07:08 +0200

    Added editTask, changeTodo and closePopup functions

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ const prepareDOMEvents = () => {
 	addBtn.addEventListener('click', addNewTask);
 	todoInput.addEventListener('keyup', enterCheck);
 	ulList.addEventListener('click', checkClick);
+	popupAddBtn.addEventListener('click', changeTodo);
+	popupCloseBtn.addEventListener('click', closePopup);
 };
 
 const addNewTask = () => {
@@ -87,19 +89,41 @@ const checkClick = (e) => {
 			e.target.closest('li').classList.toggle('completed');
 			e.target.closest('button').classList.toggle('completed');
 		} else if (e.target.closest('button').classList.contains('edit')) {
-			console.log('edit');
+			editTask(e);
 		} else if (e.target.closest('button').classList.contains('delete')) {
 			deleteTask(e);
 		}
 	}
 };
 
+const editTask = (e) => {
+	const oldTodo = e.target.closest('li').id;
+	editedTodo = document.getElementById(oldTodo);
+	popupInput.value = editedTodo.firstChild.textContent;
+	popup.style.display = 'flex';
+};
+
+const changeTodo = () => {
+	if (popupInput.value !== '') {
+		editedTodo.firstChild.textContent = popupInput.value;
+		popup.style.display = 'none';
+		popupInfo.textContent = '';
+	} else {
+		popupInfo.textContent = 'Musisz podać jakąś treść!';
+	}
+};
+
+const closePopup = () => {
+	popup.style.display = 'none';
+	popupInfo.textContent = '';
+}
+
 const deleteTask = (e) => {
 	const deleteTodo = e.target.closest('li');
 	deleteTodo.remove();
 
 	if (allTasks.length === 0) {
-		alertInfo.textContent = 'Brak zadań na liście.'
+		alertInfo.textContent = 'Brak zadań na liście.';
 	}
 };
 
